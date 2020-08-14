3e0c72225d3e6d73d1b26e43f1f828770fabef8a
harshdev92 <haggarwal25@gmail.com>
2020-08-14 22:42:41 +0530

    Updates

---
+++ precache-manifest.6f5848a436c5e007654d18a5ce70e99f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6b903bfc3f9fd00222c9923b7108069",
+    "url": "/movieListIngApp/index.html"
+  },
+  {
+    "revision": "1ffe47b9d5a9f0365ed3",
+    "url": "/movieListIngApp/static/css/2.9905d55a.chunk.css"
+  },
+  {
+    "revision": "ca48814242585a19dcb4",
+    "url": "/movieListIngApp/static/css/main.9502da91.chunk.css"
+  },
+  {
+    "revision": "1ffe47b9d5a9f0365ed3",
+    "url": "/movieListIngApp/static/js/2.f7ec41c4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movieListIngApp/static/js/2.f7ec41c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca48814242585a19dcb4",
+    "url": "/movieListIngApp/static/js/main.db9c0d1a.chunk.js"
+  },
+  {
+    "revision": "f856bd9c764e0df4bd64",
+    "url": "/movieListIngApp/static/js/runtime-main.3df7f447.js"
+  }
+]);
